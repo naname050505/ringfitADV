c55a0f706121a987966147d152d6ba346a9b36f4
naname050505 <naname84j@gmail.com>
2019-12-07 17:43:01 +0900

    add Story, Im sorry to be late

--- app/src/main/java/com/example/android/accelerometerplay/SubActivity.java
+++ app/src/main/java/com/example/android/accelerometerplay/SubActivity.java
@@ -1,12 +1,19 @@
 package com.example.android.accelerometerplay;
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,51 +21,94 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 
 public class SubActivity extends Activity {
-    public int talkcounter = 0 ;
-    public TextView talk_message;
+    private int talkcounter = 0 ;
+    private TextView talk_message;
+    private TextView touch_screen;
+    private Handler mHandler = new Handler();
+    private ScheduledExecutorService mScheduledExecutor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setScreenTalk();
         setScreenMain();
-
     }
 
     private void setScreenMain(){
         setContentView(R.layout.activity_sub);
-        final ImageView onayamiman = this.findViewById(R.id.onayamiman);
-        onayamiman.setImageResource(R.drawable.onayami);
+        touch_screen = (TextView) findViewById(R.id.touch_screen);
 
-        Button actionButton1 = findViewById(R.id.button_sub1);
-        actionButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setScreenSub3();
-            }
-        });
-        Button actionButton2 = findViewById(R.id.button_sub2);
-        actionButton2.setOnClickListener(new View.OnClickListener() {
+        Handler mHandler = new Handler();
+        ScheduledExecutorService mScheduledExecutor;
+        TextView mLblMeasuring;
+        startButton();
+        Button startButton = findViewById(R.id.touch_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setScreenTalk();
             }
         });
-        Button actionButton3 = findViewById(R.id.button_sub3);
-        actionButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setScreenSub2();
-            }
-        });
     }
+
+    private void startButton() {
+        touch_screen = (TextView) findViewById(R.id.touch_screen);
+        mScheduledExecutor = Executors.newScheduledThreadPool(2);
+
+        mScheduledExecutor.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        touch_screen.setVisibility(View.VISIBLE);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                            animateAlpha();
+                        }
+                    }
+                });
+            }
+
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+            private void animateAlpha() {
+
+                List<Animator> animatorList = new ArrayList<Animator>();
+                ObjectAnimator animeFadeIn = ObjectAnimator.ofFloat(touch_screen,
+                                                        "alpha",
+                                                             0f, 1f);
+                animeFadeIn.setDuration(1500);
+
+                ObjectAnimator animeFadeOut = ObjectAnimator.ofFloat(touch_screen,
+                                                        "alpha",
+                                                             1f, 0f);
+                animeFadeOut.setDuration(1000);
+
+                animatorList.add(animeFadeIn);
+                animatorList.add(animeFadeOut);
+
+                final AnimatorSet animatorSet = new AnimatorSet();
+                animatorSet.playSequentially(animatorList);
+                animatorSet.start();
+            }
+        }, 0, 2500, TimeUnit.MILLISECONDS);
+    }
+
+
 
     private void setScreenTalk(){
         setContentView(R.layout.activity_talk);
         talk_message = (TextView)this.findViewById(R.id.text);
+        talk_message.setTextColor(Color.WHITE);
         final ImageView onayamiman = this.findViewById(R.id.onayamiman);
         onayamiman.setImageResource(R.drawable.onayami);
+        final ImageView muscle = this.findViewById(R.id.muscle);
 
         Button actionButtonNext = findViewById(R.id.tsugihe);
 
@@ -68,40 +118,63 @@ public class SubActivity extends Activity {
                 talkcounter++;
                 switch (talkcounter){
                     case 1:
+                        onayamiman.setAlpha(100);
+                        muscle.setAlpha(255);
+                        muscle.setImageResource(R.drawable.muscle);
                         talk_message.setText(R.string.b_talk1);
                         break;
                     case 2:
+                        onayamiman.setAlpha(255);
+                        muscle.setAlpha(100);
                         talk_message.setText(R.string.a_talk2);
                         break;
                     case 3:
+                        onayamiman.setAlpha(100);
+                        muscle.setAlpha(255);
                         talk_message.setText(R.string.b_talk2);
                         break;
                     case 4:
+                        onayamiman.setAlpha(255);
+                        muscle.setAlpha(100);
                         talk_message.setText(R.string.a_talk3);
                         break;
                     case 5:
+                        onayamiman.setAlpha(100);
+                        muscle.setAlpha(255);
                         talk_message.setText(R.string.b_talk3);
                         break;
                     case 6:
-                        talkcounter = 0;
-                        setScreenSub2();
+                        setDecisionScreen();
+                        break;
                 }
             }
         });
     }
 
-    private void setScreenSub2(){
+    private void setDecisionScreen(){
         setContentView(R.layout.activity_sub2);
-        Button returnButton = findViewById(R.id.button_sub);
+        Button confirmButton = findViewById(R.id.button_sub1);
+        talk_message = (TextView)this.findViewById(R.id.text);
+        talk_message.setTextColor(Color.WHITE);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setReadyScreen();
+                talkcounter = 0;
+            }
+        });
+
+        Button returnButton = findViewById(R.id.button_sub2);
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setScreenMain();
+                setScreenTalk();
+                talkcounter = 0;
             }
         });
     }
 
-    private void setScreenSub3(){
+    private void setReadyScreen(){
         setContentView(R.layout.activity_sub3);
         final ImageView imageView2  = this.findViewById(R.id.enemy_view);
         final ImageView actionView1 = this.findViewById(R.id.action_view1);
@@ -167,42 +240,4 @@ public class SubActivity extends Activity {
         });
     }
 
-
-    /***
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_sub);
-        dash_count = this.findViewById(R.id.dash_count);
-
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-        bar = findViewById(R.id.progressBar2);
-        bar.setMax(0);
-
-        sensorManager.registerListener(new SensorEventListener() {
-            private int mCounter = 0;
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                mCounter++;
-                dash_count.setText(String.valueOf(mCounter));
-                bar.setProgress(100+(2*Integer.valueOf(dash_count.getText().toString())) );
-
-            }
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            }
-        }, sensor, SensorManager.SENSOR_DELAY_UI);
-
-        Button returnButton = (Button)findViewById(R.id.button_sub);
-        returnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), AccelerometerPlayActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-     ***/
 }
